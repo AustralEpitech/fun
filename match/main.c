edbfc55ab5e5a3d307fd534d32d6484b1e13c62d
ange <ange@yw5n.com>
2024-11-13 17:01:10 +0800

    feat: match

---
+++ match/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int match(const char *s1, const char *s2)
+{
+    return (*s2 == '*' && (match(s1, s2 + 1) || (*s1 && match(s1 + 1, s2)))) ||
+        (*s1 == *s2 && (*s1 == '\0' || match(s1 + 1, s2 + 1)));
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        return 1;
+    }
+    if (match(argv[1], argv[2])) {
+        puts("Match!");
+    } else {
+        puts("No match...");
+    }
+    return 0;
+}
